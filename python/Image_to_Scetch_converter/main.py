f3b06eb9557e5ea306c850167c902ae0e2010280
shabanvahid0101 <shabanvahid0101@gmail.com>
2026-02-04 21:50:01 +0900

    add pic to projects

--- python/Image_to_Scetch_converter/main.py
+++ python/Image_to_Scetch_converter/main.py
@@ -1,6 +1,6 @@
 import cv2
 
-image = cv2.imread('4.jpg')
+image = cv2.imread('a1.jpeg')
 gray_image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 inverted=255-gray_image
 blur=cv2.GaussianBlur(inverted,(21,21),0)
--- python/remove background/main.py
+++ python/remove background/main.py
@@ -2,7 +2,7 @@ from rembg import remove
 from PIL import Image
 
 # image address
-image_file = 'a1.jpeg'
+image_file = 'D:/code/end_project/python/remove background/1742017620713.jpg'
 # remove background image address
 image_converted = 'output.png'
 inserted_image = Image.open(image_file)
